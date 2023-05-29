8f3c8c54a83db447423a761fca0d574d3c4a56fa
Mike <mgliwicki@gmail.com>
2023-05-29 14:30:58 +0200

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ resource "azurerm_virtual_network" "consul-cluster1" {
   name                = "network-${var.name}"
   address_space       = ["10.0.0.0/16"]
   location            = "${var.location}"
-  resource_group_name = "${var.rq}"
+  resource_group_name = "${var.rg}"
 }
