7fbd5c244f374886d14e2d4d9e2a8dba26de1cdd
Mike <mgliwicki@gmail.com>
2023-05-29 12:50:15 +0200

    Update cluster-apply.tf

--- cluster-apply.tf
+++ cluster-apply.tf
@@ -6,7 +6,7 @@ variable "name" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-consul-cluster"
-    storage_account_name = "sa-consul-cluster"
+    storage_account_name = "saconsulcluster"
     container_name       = "container-consul-cluster"
     key                  = "consul-cluster.tfstate"
   }
