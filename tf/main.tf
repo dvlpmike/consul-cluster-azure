1f55153f4a934d2cc95ae46989d184baa9f34a70
Mike <mgliwicki@gmail.com>
2023-05-29 11:24:10 +0200

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,1 +1,25 @@
-...
+variable "name" {
+  type = string
+  default = "consul-cluster"
+} 
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-${var.name}"
+    storage_account_name = "sa-${var.name}"
+    container_name       = "container-${var.name}"
+    key                  = "${var.name}.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "${var.name}" {
+  name     = "rg-${var.name}"
+  location = "West Europe"
+}
