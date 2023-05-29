abf82c5151d3e4c815133fecbf71a4620de85c7d
Mike <mgliwicki@gmail.com>
2023-05-29 13:30:21 +0200

    Update cluster-apply.tf

--- cluster-apply.tf
+++ cluster-apply.tf
@@ -7,19 +7,9 @@ terraform {
   }
 }
 
-variable "name" {
-  type = string
-  default = "consul-cluster1"
-} 
-
 provider "azurerm" {
   features {}
   use_oidc = true
 }
 
 data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "consul-cluster1" {
-  name     = "rg-${var.name}"
-  location = "West Europe"
-}
