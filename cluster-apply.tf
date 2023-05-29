00b0ff079f1b7b13e681d67c7b371cf960f11fe6
Mike <mgliwicki@gmail.com>
2023-05-29 12:47:05 +0200

    Update cluster-apply.tf

--- cluster-apply.tf
+++ cluster-apply.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "$${var.name}" {
+resource "azurerm_resource_group" "consul-cluster" {
   name     = "rg-${var.name}"
   location = "West Europe"
 }
