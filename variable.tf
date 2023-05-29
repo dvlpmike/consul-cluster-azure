fa79f567d0b26addc66ae1dc9837f5a34967770a
Mike <mgliwicki@gmail.com>
2023-05-29 14:16:00 +0200

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,10 @@ variable "name" {
   type = string
   default = "consul-cluster1"
 } 
+
+variable "vm" {
+  type = object({
+    location = "azurerm_resource_group.consul-cluster1.location"
+    rg_name = "azurerm_resource_group.consul-cluster1.name"
+  })
+}
