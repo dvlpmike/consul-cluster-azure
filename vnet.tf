a183db9b689a3925da703e699e9f7a574869e262
Mike <mgliwicki@gmail.com>
2023-06-02 14:15:07 +0200

    Delete vnet.tf

--- vnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "consul-cluster1" {
-  name                = "network-${var.name}"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${var.location}"
-  resource_group_name = "${var.rg}"
-}
