ffed50ca33b999a8f070cb6526a4da359d01dcd7
Mike <mgliwicki@gmail.com>
2023-06-02 13:29:26 +0200

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -4,14 +4,14 @@ resource "azurerm_linux_virtual_machine" "consul-cluster1" {
   resource_group_name = "${var.rg}"
   location            = "${var.location}"
   size                = "Standard_F2"
-  admin_username      = "${var.user}"
+  admin_username      = "${var.ssh_user}"
   network_interface_ids = [
     azurerm_network_interface.consul-cluster1.id,
   ]
 
   admin_ssh_key {
-    username   = "${var.user}"
-    public_key = "${var.publickey}"
+    username   = "${var.ssh_user}"
+    public_key = "${var.public_key}"
   }
 
   os_disk {
