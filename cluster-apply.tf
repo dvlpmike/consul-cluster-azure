92dfc237cb2e290fcee083924bb876717c77cde6
Mike <mgliwicki@gmail.com>
2023-06-02 22:28:55 +0200

    Update cluster-apply.tf

--- cluster-apply.tf
+++ cluster-apply.tf
@@ -19,12 +19,12 @@ resource "azurerm_resource_group" "consul-cluster1" {
   location = "${var.location}"
 }
 
-#resource "azurerm_virtual_network" "consul-cluster1" {
-#  name                = "network-${var.name}"
-#  address_space       = ["10.0.0.0/16"]
-#  location            = "${var.location}"
-#  resource_group_name = "${var.rg}"
-#}
+resource "azurerm_virtual_network" "consul-cluster1" {
+  name                = "network-${var.name}"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.rg
+}
 
 #resource "azurerm_subnet" "consul-cluster1" {
 #  name                 = "subnet-${var.name}"
