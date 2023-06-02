8848af40da6669f285c1a49a890d7ce26242dce3
Mike <mgliwicki@gmail.com>
2023-06-02 22:35:26 +0200

    Update cluster-apply.tf

--- cluster-apply.tf
+++ cluster-apply.tf
@@ -33,19 +33,19 @@ resource "azurerm_subnet" "consul-cluster1" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-#resource "azurerm_network_interface" "consul-cluster1" {
-#  count               = 3
-#  name                = "nic-${var.name}${count.index}"
-#  location            = "${var.location}"
-#  resource_group_name = "${var.rg}"
-#
-#  ip_configuration {
-#    name                          = "internal-network-${var.name}"
-#    subnet_id                     = azurerm_subnet.consul-cluster1.id
-#    private_ip_address_allocation = "Dynamic"
-#  }
-#}
-#
+resource "azurerm_network_interface" "consul-cluster1" {
+  count               = 3
+  name                = "nic-${var.name}${count.index}"
+  location            = var.location
+  resource_group_name = var.rg
+
+  ip_configuration {
+    name                          = "internal-network-${var.name}"
+    subnet_id                     = azurerm_subnet.consul-cluster1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 #resource "azurerm_linux_virtual_machine" "consul-cluster1" {
 #  count               = 3
 #  name                = "${var.name}${count.index}"
