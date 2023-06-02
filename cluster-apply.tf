57c65f96c4ed583f9dce44f415e11f3c12ae86ff
Mike <mgliwicki@gmail.com>
2023-06-02 22:52:47 +0200

    Update cluster-apply.tf

--- cluster-apply.tf
+++ cluster-apply.tf
@@ -68,7 +68,7 @@ resource "azurerm_linux_virtual_machine" "consul-cluster1" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
